0363d9616b388a9051286cde77e8886a72ad57a0
Yasiru <yasirunipunbasnayake@gmail.com>
2025-10-06 14:52:44 +0530

    Add notification-service with RabbitMQ integration and remove MongoDB dependencies

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,50 +1,23 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const { mongo, default: mongoose } = require('mongoose')
-const app = express()
-const port = 3001
+const amqp = require('amqplib');
 
-app.use(bodyParser.json())
-
-mongoose.connect('mongodb://mongo:27017/tasks').then(() => { 
-    console.log('Connected to MongoDB')
-}).catch(err => {
-    console.error('Failed to connect to MongoDB', err)
-})
-
-const taskSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    userId: String,
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
-    }
-})
-
-const Task = mongoose.model('Task', taskSchema)
-
-app.post('/tasks', async (req, res) => {
-    const {title, description, userId} = req.body;
+async function start() {
     try {
-        const task = new Task({title, description, userId});
-        await task.save();
-        res.status(201).send(task);
+        const connection = await amqp.connect('amqp://rabbitmq:5672');
+        const channel = await connection.createChannel();
+        await channel.assertQueue('task_created');
+        console.log('Notification Service connected to RabbitMQ');
+
+        channel.consume('task_created', msg => {
+            if (msg !== null) {
+                const messageContent = msg.content.toString();
+                const task = JSON.parse(messageContent);
+                console.log(`New Task Created: [ID: ${task.id}, Title: ${task.title}, UserID: ${task.userId}]`);
+                channel.ack(msg);
+            }
+        });
     } catch (error) {
-        res.status(500).send(error);
+        console.error('Failed to connect to RabbitMQ', error);
     }
-});
+}
 
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.json(tasks);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-app.listen(port, () => {
-  console.log(`Task Service app listening on port ${port}`)
-})
-
+start();
